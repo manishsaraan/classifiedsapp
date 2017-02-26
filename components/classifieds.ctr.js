f77c0d539ae30c06c56536d8abce5242be1bc0c4
manish saraan <manish021js@gmail.com>
2017-02-26 20:01:36 +0530

    changes for router and admin functionality

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -1,18 +1,68 @@
 (function(){
         "use strict";
         angular.module("ngClassifieds")
-        	   .controller("classifiedsCtrl",["$scope","$http","classifiedsFactory","$mdSidenav",function($scope,$http,classifiedsFactory,$mdSidenav){
+        	   .controller("classifiedsCtrl",["$scope","$http","classifiedsFactory","$mdSidenav","$mdToast","$mdDialog",function($scope,$http,classifiedsFactory,$mdSidenav,$mdToast,$mdDialog){
                    
                          $scope.classifieds ={};
                          classifiedsFactory.getClassifieds().then(function(classifieds){
                          	 $scope.classifieds = classifieds.data;
                          });
+                         var contact = {
+                         	name:"manish",
+                         	phone:"85214702",
+                         	email:"manish@mail.com"
+                         };
                          $scope.openSidebar =function(){
                              $mdSidenav('left').open();
                          };
                          $scope.closeSidebar =function(){
                              $mdSidenav('left').close();
                          };
+                         $scope.saveClassified = function(classified){
+                         	 if(classified){
+                         	 	classified.contact = contact;
+                         	 	classified.created = new Date().getTime();
+                                 $scope.classifieds.push(classified);
+                                 $scope.classified = {};
+                                  $mdSidenav('left').close();
+                                  showToast('classified Saved !!!');
+                         	 }
+                         };
+                         function showToast(msg){
+                         	$mdToast.show(
+                                  	$mdToast.simple()
+                                            .content(msg)
+                                            .position("top,left")
+                                            .hideDelay("3000")
+                                  	);
+                         }
+                         $scope.saveEdit = function(){
+                         	$scope.editing =false;
+                         	$scope.closeSidebar();
+                            showToast('classified Edited Successfully !!!');
+                         };
+                         $scope.editClassified = function(editClassified){
+                         	$scope.editing =true;
+                         	$mdSidenav('left').open();
+                         	$scope.classified = editClassified;
+                         }
+                         $scope.deleteClassified = function(event,classified){
+                         	var confirm  = $mdDialog.confirm()
+                         	               .title("Are you sure you want to delete '"+classified.title+"' ?")
+                         	               .ok('yes')
+                         	               .cancel("Nope")
+                         	               .targetEvent(event);
+                            	            $mdDialog.show(confirm).then(function(){
+									    	var index = $scope.classifieds.indexOf(classified);
+										    $scope.classifieds.splice(index,1);
+										    showToast('classified Deleted Successfully !!!');
+                         	          },function(){
+
+                         	          });
+                         	
+                         	   
+                         }
         	   }]);
+
 
 })();
--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,15 @@
-angular.module("ngClassifieds",["ngMaterial"])
-       .config(function($mdThemingProvider){
+angular.module("ngClassifieds",["ngMaterial","ui.router"])
+       .config(function($mdThemingProvider,$stateProvider, $urlRouterProvider){
                $mdThemingProvider.theme("default")
                                  .primaryPalette('teal')
                                  .accentPalette('orange');
-       })
-      ;
+                               $urlRouterProvider.otherwise("/classifieds");
+               $stateProvider
+              .state('classifieds', {
+		            url: '/classifieds',
+		            templateUrl: 'components/classifieds/classifieds.tpl.html',
+		            controller:"classifiedsCtrl"
+		        })
+		        
+		       
+       }) ;
